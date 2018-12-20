78ca43d406af01f7ca6c814b0f2bf3be2dad85b6
ThibautLB <thibaut56190@gmail.com>
2018-12-20 09:48:20 +0100

    add sql

--- backend/bdd.php
+++ backend/bdd.php
@@ -41,12 +41,47 @@ if($_POST['action'] == "init")
 	`id` INT NOT NULL AUTO_INCREMENT,
 	`Nom` TEXT NOT NULL,
 	`img_id` INT NOT NULL,
-	`duree` INT,
-	`difficultee` INT,
+	`duree` INT NOT NULL,
+	`difficultee` INT NOT NULL,
 	`description` TEXT NOT NULL,
-	`quantite` INT,
-	`genre` INT,
+	`quantite` INT NOT NULL,
+	`genre` INT NOT NULL,
   primary key (id)
+);";
+  $sql = "CREATE TABLE IF NOT EXISTS `Ingredient` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `ingredient` TEXT NOT NULL,
+  `prix` INT unsigned,
+  `nutrition` TEXT,
+  PRIMARY KEY (`id`)
+);";
+  $sql = "CREATE TABLE IF NOT EXISTS `Utilisateur` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Nom` TEXT NOT NULL,
+  `Prenom` TEXT NOT NULL,
+  `Email` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);";
+  $sql = "CREATE TABLE IF NOT EXISTS `Note` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `note` FLOAT unsigned,
+  PRIMARY KEY (`id`)
+);";
+  $sql = "CREATE TABLE If NOT EXISTS `Pays` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Nom` FLOAT unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+);";
+  $sql = "CREATE TABLE If NOT EXISTS `Ustensile` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `Nom` FLOAT unsigned NOT NULL,
+  `prix` INT unsigned,
+  PRIMARY KEY (`id`)
+);";
+  $sql = "CREATE TABLE If NOT EXISTS `Recette-note` (
+  `id-Recette` INT,
+  `id-Note` INT,
+  PRIMARY KEY (`id-Recette`,`id-Note`)
 );";
 
   if ($conn->query($sql) === TRUE) {
